06277d723551bd52362b8372e0b746e6a998fb62
Bruno de Faria <118024873+bfaria81@users.noreply.github.com>
2023-09-20 22:13:09 -0300

    Add files via upload

---
+++ src/main/java/GestaoReservaNotebooks/Reservas/Controller/C_CadUsuario.java
@@ -0,0 +1,12 @@
+package GestaoReservaNotebooks.Reservas.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class C_CadUsuario {
+    @GetMapping("/cadastro")
+    public String getCadastro(){
+    return "cadastros/usuario";
+    }
+}
